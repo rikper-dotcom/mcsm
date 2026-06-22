8c41dc2e555e87365578b9b9dafe3715db3dd745
Rikard Persson <DIN_GITHUB_EMAIL>
2026-06-22 20:02:43 +0200

    feat: initialize MCSM project

--- src/mcsm/cli.py
+++ src/mcsm/cli.py
@@ -1,5 +1,8 @@
+import platform
+
 import typer
 
+from mcsm.console import console
 from mcsm.version import VERSION
 
 app = typer.Typer(
@@ -8,12 +11,18 @@ app = typer.Typer(
 )
 
 
-@app.command()
-def version() -> None:
-    """Show version information."""
-    print(f"Minecraft Server Manager v{VERSION}")
-
-
 @app.callback()
 def main() -> None:
     """Minecraft Server Manager."""
+
+
+@app.command()
+def version() -> None:
+    """Show version information."""
+
+    console.print()
+    console.print("[bold green]Minecraft Server Manager[/bold green]")
+    console.print(f"Version : [cyan]{VERSION}[/cyan]")
+    console.print(f"Python  : [yellow]{platform.python_version()}[/yellow]")
+    console.print(f"System  : [magenta]{platform.system()}[/magenta]")
+    console.print()
